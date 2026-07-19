c30ae3d8faedd26c129f616da9aae16212c993a8
Francisco Santos <franciscoabasantos@gmail.com>
2026-07-20 00:38:01 +0100

    Update Browser search

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,10 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  allowedDevOrigins: [
+    "127.0.0.1",
+    "172.23.117.103",
+  ],
+};
 
 export default nextConfig;
